9d999bab57368a7f979c75dc6dc97313cda1d356
neelkapse <neelkapse@gmail.com>
2015-09-19 16:35:54 -0400

    Added accordion list groups, modified colors.

--- popup.js
+++ popup.js
@@ -1,7 +1,3 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 /**
  * Get the current URL.
  *
@@ -14,14 +10,12 @@ function getCurrentTabUrl(callback) {
         active: true,
         currentWindow: true
     };
-
     chrome.tabs.query(queryInfo, function(tabs) {
   
         var tab = tabs[0];
         var url = tab.url;
   
     console.assert(typeof url == 'string', 'tab.url should be a string');
-
     callback(url);
     });
 }
@@ -34,54 +28,6 @@ function getRecList() {
 	var p = JSON.parse(data);
 }
 
-/**
- * Gets the list of websites recommended for the user from database
- */
-function getRecList() {
-
-}
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-    // Google image search - 100 searches per day.
-    // https://developers.google.com/image-search/
-    var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' + 
-        '?v=1.0&q=' + encodeURIComponent(searchTerm);
-    var x = new XMLHttpRequest();
-    x.open('GET', searchUrl);
-    // The Google image search API responds with JSON, so let Chrome parse it.
-    x.responseType = 'json';
-    x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    	var response = x.response;
-    	if (!response || !response.responseData || !response.responseData.results ||
-        	response.responseData.results.length === 0) {
-     			errorCallback('No response from Google Image search!');
-          		return;
-        	}
-        var firstResult = response.responseData.results[0];
-        // Take the thumbnail instead of the full image to get an approximately
-        // consistent image size.
-        var imageUrl = firstResult.tbUrl;
-        var width = parseInt(firstResult.tbWidth);
-        var height = parseInt(firstResult.tbHeight);
-        console.assert(
-            typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-            'Unexpected respose from the Google Image Search API!');
-        callback(imageUrl, width, height);
-    };
-    x.onerror = function() {
-      errorCallback('Network error.');
-    };
-    x.send();
-}
-
 function renderStatus(statusText) {
 	document.getElementById('status').textContent = statusText;
 }
@@ -89,12 +35,8 @@ function renderStatus(statusText) {
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
     // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
+    renderStatus('Adding info to database for ' + url);
 
-    getImageUrl(url, function(imageUrl, width, height) {
-
-    	renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
     	var imageResult = document.getElementById('image-result');
       	// Explicitly set the width/height to minimize the number of reflows. For
       	// a single image, this does not matter, but if you're going to embed
