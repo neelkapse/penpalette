637799192bbd693c38835073a30a211909f16897
Neel Kapse <neelkapse@dhcp-ccc-8539.redrover.cornell.edu>
2015-09-18 23:24:31 -0400

    Initial file upload

---
+++ popup.js
@@ -0,0 +1,112 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function(tabs) {
+  
+        var tab = tabs[0];
+        var url = tab.url;
+  
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+    });
+}
+
+/**
+ * Gets the list of websites recommended for the user from database
+ */
+function getRecList() {
+	var data = databaseCall();		// TODO
+	var p = JSON.parse(data);
+}
+
+/**
+ * Gets the list of websites recommended for the user from database
+ */
+function getRecList() {
+
+}
+
+/**
+ * @param {string} searchTerm - Search term for Google Image search.
+ * @param {function(string,number,number)} callback - Called when an image has
+ *   been found. The callback gets the URL, width and height of the image.
+ * @param {function(string)} errorCallback - Called when the image is not found.
+ *   The callback gets a string that describes the failure reason.
+ */
+function getImageUrl(searchTerm, callback, errorCallback) {
+    // Google image search - 100 searches per day.
+    // https://developers.google.com/image-search/
+    var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' + 
+        '?v=1.0&q=' + encodeURIComponent(searchTerm);
+    var x = new XMLHttpRequest();
+    x.open('GET', searchUrl);
+    // The Google image search API responds with JSON, so let Chrome parse it.
+    x.responseType = 'json';
+    x.onload = function() {
+    // Parse and process the response from Google Image Search.
+    	var response = x.response;
+    	if (!response || !response.responseData || !response.responseData.results ||
+        	response.responseData.results.length === 0) {
+     			errorCallback('No response from Google Image search!');
+          		return;
+        	}
+        var firstResult = response.responseData.results[0];
+        // Take the thumbnail instead of the full image to get an approximately
+        // consistent image size.
+        var imageUrl = firstResult.tbUrl;
+        var width = parseInt(firstResult.tbWidth);
+        var height = parseInt(firstResult.tbHeight);
+        console.assert(
+            typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
+            'Unexpected respose from the Google Image Search API!');
+        callback(imageUrl, width, height);
+    };
+    x.onerror = function() {
+      errorCallback('Network error.');
+    };
+    x.send();
+}
+
+function renderStatus(statusText) {
+	document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getCurrentTabUrl(function(url) {
+    // Put the image URL in Google search.
+    renderStatus('Performing Google Image search for ' + url);
+
+    getImageUrl(url, function(imageUrl, width, height) {
+
+    	renderStatus('Search term: ' + url + '\n' +
+          'Google image search result: ' + imageUrl);
+    	var imageResult = document.getElementById('image-result');
+      	// Explicitly set the width/height to minimize the number of reflows. For
+      	// a single image, this does not matter, but if you're going to embed
+      	// multiple external images in your page, then the absence of width/height
+      	// attributes causes the popup to resize multiple times.
+      	imageResult.width = width;
+      	imageResult.height = height;
+      	imageResult.src = imageUrl;
+      	imageResult.hidden = false;
+
+    }, function(errorMessage) {
+      renderStatus('Cannot display image. ' + errorMessage);
+    });
+  });
+});
